6299be0ed6c26e2ebc780239219e7efad9deb0aa
ericrothmuller <eric@skymarketingco.com>
2023-08-11 00:05:50 -0700

    Added product divs with stylings to shop page.

--- fireflyathletics/src/pages/shop.js
+++ fireflyathletics/src/pages/shop.js
@@ -5,6 +5,12 @@ return (
 	<main className="pageHeadingDiv">
 	<h1 className="pageTitle">Shop Firefly Athletics</h1>
 	<p className="pageParagraph">Items go here.</p>
+        <div className="productDiv">
+        <div className="product">Product 1</div>
+        <div className="product">Product 2</div>
+        <div className="product">Product 3</div>
+        <div className="product">Product 4</div>
+    </div>
 	</main>
 );
 };

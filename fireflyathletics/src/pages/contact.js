2ef194a7a305ba9d1d4d2d41352dad512c7a12e5
ericrothmuller <eric@skymarketingco.com>
2023-08-10 19:31:33 -0700

    Added base code for home.js and contact.js page components.

---
+++ fireflyathletics/src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+return (
+    <main className="pageHeadingDiv">
+        <h1 className="pageTitle">Get in Touch</h1>
+        <p className="pageParagraph">Paragraph</p>
+        <p className="pageParagraph">Paragraph</p>
+    </main>
+);
+};
+
+export default Contact;
---
+++ fireflyathletics/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+return (
+	<main className="pageHeadingDiv">
+	<h1 className="pageTitle">Welcome to Firefly Athletics</h1>
+	<p className="pageParagraph">We are an athletics apparel and fitness equipment company.</p>
+	</main>
+);
+};
+
+export default Home;

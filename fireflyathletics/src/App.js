15d30571772ed9500d48cee2c2ec23b92c855af4
ericrothmuller <eric@skymarketingco.com>
2023-08-10 19:52:12 -0700

    Installed NPM packages navbar, react-icons, and styled-components and completed Navbar folder connecting the home.js and contact.js pages to the Navbar.

--- fireflyathletics/src/App.js
+++ fireflyathletics/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route}
@@ -7,16 +7,16 @@ import Home from './pages/home';
 import Contact from './pages/contact';
 
 function App() {
-  return (
-    <Router>
-    <Navbar />
-    <Routes>
-      <Route exact path='/' element={<Home/>} />
-      <Route path='/home' element={<Home/>} />
-      <Route path='/contact' element={<Contact/>} />
-    </Routes>
-    </Router>
-  );
-  }
-  
-  export default App;
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+    <Route exact path='/' element={<Home/>} />
+    <Route path='/home' element={<Home/>} />
+		<Route path='/contact' element={<Contact/>} />
+	</Routes>
+	</Router>
+);
+}
+
+export default App;
---
+++ fireflyathletics/src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Nav, NavLink, NavMenu }
+    from "./NavbarElements";
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="/home">
+            Home
+          </NavLink>
+          <NavLink to="/contact">
+            Contact Us
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;

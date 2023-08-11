c21c4bf5697391a36bcf8f6995f99619d72d3713
ericrothmuller <eric@skymarketingco.com>
2023-08-11 09:33:23 -0700

    Renamed shopButton class to productButton class because I'm planning on changing the button call to action and want a more universally recognized class name for the button.

--- fireflyathletics/src/pages/shop.js
+++ fireflyathletics/src/pages/shop.js
@@ -10,22 +10,22 @@ return (
         <div className="product">
             <h2>Premium Yoga Mat</h2>
         <img src={falogo} className="singleProduct" alt="logo" />
-        <p><button className="shopButton">Shop</button></p>
+        <p><button className="productButton">Shop</button></p>
         </div>
         <div className="product">
             <h2>Lightweight Yoga Mat</h2>
             <img src={falogo} className="singleProduct" alt="logo" />
-            <p><button className="shopButton">Shop</button></p>
+            <p><button className="productButton">Shop</button></p>
             </div>
         <div className="product">
             <h2>Yoga Blocks</h2>
             <img src={falogo} className="singleProduct" alt="logo" />
-            <p><button className="shopButton">Shop</button></p>
+            <p><button className="productButton">Shop</button></p>
         </div>
         <div className="product">
             <h2>Resistance Bands</h2>
             <img src={falogo} className="singleProduct" alt="logo" />
-            <p><button className="shopButton">Shop</button></p>
+            <p><button className="productButton">Shop</button></p>
         </div>
     </div>
 	</main>

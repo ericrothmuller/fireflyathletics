4418786906f83d8f92c1d1f6c709020bf5737ff4
ericrothmuller <eric@skymarketingco.com>
2023-08-30 22:58:25 -0700

    Added contact info to contact page

--- fireflyathletics/src/pages/contact.js
+++ fireflyathletics/src/pages/contact.js
@@ -4,8 +4,8 @@ const Contact = () => {
 return (
     <main className="pageHeadingDiv">
         <h1 className="pageTitle">Get in Touch</h1>
-        <p className="pageParagraph">Paragraph</p>
-        <p className="pageParagraph">Paragraph</p>
+        <p className="pageParagraph">Email: eric@fireflyathletics.com</p>
+        <p className="pageParagraph">Phone: 707-843-2047</p>
     </main>
 );
 };

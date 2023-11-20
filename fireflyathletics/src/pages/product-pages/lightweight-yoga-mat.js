3ef71085a6066768ff82c98fb8fa429d19b27b8a
ericrothmuller <eric@skymarketingco.com>
2023-11-20 11:43:55 -0800

    Updated lightweightyoga mat text

--- fireflyathletics/src/pages/product-pages/lightweight-yoga-mat.js
+++ fireflyathletics/src/pages/product-pages/lightweight-yoga-mat.js
@@ -4,7 +4,7 @@ const LightweightYogaMat = () => {
     return (
         <main className="pageHeadingDiv">
         <h1 className="pageTitle">Lightweight Yoga Mat</h1>
-        <p className="pageParagraph">Product info goes here.</p>
+        <p className="pageParagraph">Product info goes right here.</p>
         </main>
     );
 };
